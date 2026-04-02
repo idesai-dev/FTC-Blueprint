07443434a0800726ec8d187879815b77d14c5b1b
idesai-dev <idesai.dev@gmail.com>
2026-04-02 18:24:40 -0500

    Public Version Update

--- src/routes/hardware/+page.ts
+++
@@ -1,6 +0,0 @@
-import { getAllPosts } from '$lib/utils/posts';
-
-export async function load() {
-	const posts = await getAllPosts();
-	return { posts: posts.filter(p => (p.meta.tags || []).includes('hardware')) };
-}
